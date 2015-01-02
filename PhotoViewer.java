717429c20da97056aab46a9a60b65eb4572038b6
Jordan Waughtal <jmwaughtal1@gmail.com>
2015-01-02 13:50:04 -0600

    Added a status bar with its function

--- PhotoViewer.java
+++ PhotoViewer.java
@@ -3,25 +3,42 @@ import java.nio.file.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.awt.*;
+import javax.swing.border.*;
 
 public class PhotoViewer extends JFrame implements ActionListener
 {
     JMenuItem about, openPhoto;
     public final String sep = File.separator;
-    JLabel ImageHolder;
+    JLabel ImageHolder, status;
     public PhotoViewer()
     {
         super("Photo Viewer");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(800,600);
+        setLayout(new BorderLayout());
         setJMenuBar(menu());
         add(photoContainer());
+        add(statusBar(), BorderLayout.SOUTH);
         setVisible(true);
     }
     private JLabel photoContainer()
     {
-        ImageHolder = new JLabel("Open a photo to view it.");
+        ImageHolder = new JLabel("");
         return ImageHolder;
+    }
+    private JPanel statusBar()
+    {
+        JPanel res = new JPanel();
+        res.setBorder(new BevelBorder(BevelBorder.LOWERED));
+        res.setPreferredSize(new Dimension(this.getWidth(), 16));
+        res.setLayout(new BoxLayout(res, BoxLayout.X_AXIS));
+        
+        status = new JLabel("Open a photo to view it.");
+        status.setHorizontalAlignment(SwingConstants.LEFT);
+        
+        res.add(status);
+        
+        return res;
     }
     private JMenuBar menu()
     {
@@ -58,8 +75,8 @@ public class PhotoViewer extends JFrame implements ActionListener
         fileChoose.showDialog(this, "Open Photo");
         File chosen = fileChoose.getSelectedFile();
         ImageIcon res = new ImageIcon(chosen.toString());
-        System.out.println("DEBUG \n"+chosen.toString());
         ImageHolder.setIcon(res);
+        status.setText(chosen.toString());
     }
     
     public static void main(String[] args)
